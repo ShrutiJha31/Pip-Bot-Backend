f31aec60fa58132708ec51cc0dbd9001b0041746
ShrutiJha31 <2019ugcs013@nitjsr.ac.in>
2021-02-01 20:15:35 +0530

    adding website handling routes

--- config/db_config.js
+++ config/db_config.js
@@ -2,12 +2,16 @@
 require('dotenv').config()
 const util = require("util");
 const mysql = require("mysql");
+const fs=require('fs');
+const path=require('path');
 
 let dbConn = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+   multipleStatements: true
+
 });
 
 dbConn.getConnection((err, connection) => {
@@ -15,6 +19,15 @@ dbConn.getConnection((err, connection) => {
     console.log(err);
   } else {
     console.log("Connected to Database");
+    const sql = fs.readFileSync(path.join(__dirname, './../db.sql')).toString();
+    connection.query(sql, function(err, result){
+      
+      if(err){
+          console.log('error: ', err);
+          
+      }
+      
+  });
   }
   if (connection) connection.release();
   return;
