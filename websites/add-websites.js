b88c8af3547670571b115270d0cc275b6b4c475a
Shruti Jha <2019ugcs013@nitjsr.ac.in>
2021-02-07 15:12:58 +0530

    Update add-websites.js

--- websites/add-websites.js
+++ websites/add-websites.js
@@ -1,31 +1,96 @@
-const express=require("express");
-const Router=express.Router();
-var pool=require('./../config/db_config')
+const express = require("express");
+const Router = express.Router();
+var pool = require('./../config/db_config')
 var VerifyToken = require('../auth/VerifyToken');
 
+const https = require('https');
+Router.post('/', VerifyToken, function (req, res) {
 
-Router.post('/',VerifyToken, function (req, res) {
+	pool.getConnection((err, connection) => {
+		if (err) throw err
 
-        pool.getConnection((err, connection) => {
-                if(err) throw err
-                
-                
-                if (req.body.user_id!=req.userId) return res.status(401).send({error:true, message:'Authentication Error'});
-                connection.query('INSERT INTO websites SET ?',[req.body ], (err, rows) => {
-                connection.release() 
-                if (!err) {
-                    res.send(rows)
-                    console.log(rows)
-                    console.log("Inserted Successfully!")                             
-                } else {
-                    console.log(err)
-                }
-                
-            
-        
-                })
-            })
-}); 
+
+		let interval = req.body.interval;
+		var link = req.body.link;
+		if (req.body.user_id != req.userId) return res.status(401).send({
+			error: true,
+			message: 'Authentication Error'
+		});
+		connection.query('INSERT INTO websites SET ?', [req.body], (err, result) => {
+			connection.release()
+			if (!err) {
+				res.send(result)
+
+				console.log(result);
+				let id = 0;
+				connection.query('SELECT COUNT(*) FROM websites', (err, result) => {
+					id = result[0]['COUNT(*)'];
+					console.log(id);
+				});
+				console.log("Inserted Successfully!" + interval);
+
+				this.interval = setInterval(() => {
+
+
+					let sql = 'Select * FROM websites WHERE user_id =? ';
+
+					pool.query(sql, [req.userId],
+						(err, result) => {
+
+							if (err) {
+
+								console.log(error)
+							} else {
+
+
+								var website_id = id;
+								var start = new Date();
+								https.get(`${link}`, function (res, err) {
+									var responseTime = new Date() - start;
+									var statusCode = 404;
+									if (!err) {
+										const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+
+										statusCode = res.statusCode;
+
+
+										console.log("StatusChecked" + website_id + " " + statusCode);
+									} else {
+										statusCode = 404;
+									}
+
+									let sql = 'INSERT INTO logs SET website_id = ? , status = ? , response_time =?';
+
+									pool.query(sql, [website_id, statusCode, responseTime],
+										(err, result) => {
+
+											if (err) {
+
+												console.error(err);
+											} else
+												console.log(result);
+										});
+
+								}).on('error', function (e) {
+									console.error(e);
+
+								});
+
+							}
+
+
+						}
+					)
+				}, interval);
+
+			} else {
+				console.log(err)
+			}
+
+
+		})
+	})
+});
 
 
 module.exports = Router;
