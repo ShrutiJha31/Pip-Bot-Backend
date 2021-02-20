302555ba988aeb99669aaddc349ae01e46da2ad4
ASHISH KUMAR <ashishk8581@gmail.com>
2021-02-20 18:15:58 +0530

    added curl

--- websites/add-websites.js
+++ websites/add-websites.js
@@ -1,30 +1,23 @@
 const express=require("express");
 const Router=express.Router();
-var pool=require('./../config/db_config')
+// var pool=require('./../config/db_config')
 var VerifyToken = require('../auth/VerifyToken');
+var dbConn = require('../config/db_config');
 
-
-Router.post('/',VerifyToken, function (req, res) {
-
-        pool.getConnection((err, connection) => {
-                if(err) throw err
-                
-                
-                if (req.body.user_id!=req.userId) return res.status(401).send({error:true, message:'Authentication Error'});
-                connection.query('INSERT INTO websites SET ?',[req.body ], (err, rows) => {
-                connection.release() 
+Router.post('/',VerifyToken,function (req, res) {
+                const body = req.body
+                const userid = req.userId
+                dbConn.query("INSERT INTO websites (website_name,tags,link,intervals,user_id) VALUES (?,?,?,?,?) ",
+                [body.website_name, body.tags, body.link, body.interval, userid],
+               (err, rows) => {
+               
                 if (!err) {
                     res.send(rows)
                     console.log(rows)
                     console.log("Inserted Successfully!")                             
                 } else {
                     console.log(err)
-                }
-                
-            
-        
-                })
-            })
+                }})
 }); 
 
 
